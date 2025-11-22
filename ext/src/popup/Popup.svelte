12b4a01cfcfeacb5d68ab53e7a095ccab8b59adc
plw <kwazzartcreature@gmail.com>
2025-11-22 03:00:05 -0800

    ext first tweak

--- ext/src/popup/Popup.svelte
+++ ext/src/popup/Popup.svelte
@@ -5,6 +5,8 @@
 
 	onMount(() => {
 		console.log('Popup mounted');
+		const theme = localStorage.getItem('theme') || 'DARK';
+		document.documentElement.setAttribute('data-theme', theme);
 	});
 
 	let count = $state(0);
@@ -13,5 +15,14 @@
 <main class="min-w-[380px] min-h-[560px] p-4">
 	<h3>Svelte Popup</h3>
 	<button onclick={() => count++}>Clicked {count} times</button>
+	<button
+		onclick={() => {
+			const theme = document.documentElement.getAttribute('data-theme') || 'DARK';
+			document.documentElement.setAttribute('data-theme', theme === 'DARK' ? 'LIGHT' : 'DARK');
+		}}
+	>
+		Toggle Theme
+	</button>
 	<img src={favicon} alt="Logo" class="w-10 h-10" />
+	<p class="text-primary">Current theme: {document.documentElement.getAttribute('data-theme')}</p>
 </main>
